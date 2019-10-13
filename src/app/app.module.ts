3d2dce462dbc27e99ae2b581a0c355e772c8849a
Andres Gesteira <gesteira.pro@gmail.com>
2019-10-13 17:03:08 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 // import * as fromComponents from './components';
 import * as fromContainers from './containers';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [...fromContainers.containers],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
   bootstrap: [fromContainers.AppComponent]
 })
 export class AppModule {}
