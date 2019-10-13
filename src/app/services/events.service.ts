52849ec67fd271aca61c57d554691f95516cbfbc
Andres Gesteira <gesteira.pro@gmail.com>
2019-10-13 18:12:43 +0200

    order events by date desc

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Event } from '../models/event.model';
 import { ErrorService } from './error.service';
 
@@ -15,6 +15,9 @@ export class EventService {
     const url = `events`;
 
     return this.http.get(url).pipe(
+      map((events: Event[]) => {
+        return events.sort((a, b) => b.date - a.date);
+      }),
       tap((val: Event[]) => console.log(val)),
       catchError(this.errorService.handleHttpError('getEvents'))
     );
